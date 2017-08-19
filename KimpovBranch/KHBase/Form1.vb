75f1d0db41e1867bc7977f5ff818454088c6a712
ChunLynda1 <chunlynda@outlook.com>
2017-08-19 19:13:50 +0700

    edit by lynda

--- KimpovBranch/KHBase/Form1.vb
+++ KimpovBranch/KHBase/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
 
     Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
-        MessageBox.Show("you clicked me!")
+        MessageBox.Show("you clicked me edit by Lynda!")
     End Sub
 End Class
