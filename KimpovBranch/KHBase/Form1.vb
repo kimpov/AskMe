074ecf25c4fc8f0ee10855f27d4a0ec88d833907
tolakh <montola.mt@gmail.com>
2017-08-19 19:14:31 +0700

    edit by tola

--- KimpovBranch/KHBase/Form1.vb
+++ KimpovBranch/KHBase/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
 
     Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
-        MessageBox.Show("you clicked me!")
+        MessageBox.Show("you clicked me! Edit by tola")
     End Sub
 End Class
